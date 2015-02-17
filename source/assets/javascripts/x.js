ebb215420cef639da8d729d37f70d50bfb2eaa48
Alena Soldatova <marsana@bk.ru>
2015-02-17 09:48:22 +0000

    open people after filter

--- source/assets/javascripts/x.js
+++ source/assets/javascripts/x.js
@@ -40,23 +40,6 @@ function changeHashWithoutScrolling(hash) {
 
 
 $(document).ready(function(){
-  // document.addEventListener('backbutton', function(){
-  // });
-  // $(window).keyup(function(e){
-  //   if(e.keyCode == 8){
-  //     if(location){
-  //       console.log($(location  ));
-  //       setTimeout(function() {
-  //         var idtop = $('.what').offset().top-200;
-  //         $(window).scrollTop(idtop);
-  //         setTimeout(function() {
-  //           $(location).trigger( "click" );
-  //           $(window).trigger('scroll');
-  //         }, 1000);
-  //       }, 100);
-  //     }
-  //   }
-  // })
   var location = window.location.hash, buffer_hash;
   var item,project,menu,post_id,to,
     CLASS_ACTIVE = 'active',
@@ -156,7 +139,19 @@ $(document).ready(function(){
     mainClass: 'mfp-fade',
     callbacks: {
       beforeOpen: function(){
-      //--------------------------------------Отмена фильтрации при открытии--------------------------------------
+
+        var magnificPopup = $.magnificPopup.instance,
+                cur = magnificPopup.st.el;
+
+              $currContainer = cur.parent();
+                var currArr;
+
+        if (cur.hasClass('people-item')) {
+            currArr = peopleArr;
+            $item.css('z-index','2');
+
+          }else {
+                  //--------------------------------------Отмена фильтрации при открытии--------------------------------------
         var flag_status;
         $('.filter_active').removeClass('filter_active');
         $item.removeClass('filtred');
@@ -167,20 +162,6 @@ $(document).ready(function(){
         $('.item').removeClass('display-none');
         if (init($container, [], itemsArr )) init( $peopleItems, [], peopleArr );
       //--------------------------------------***--------------------------------------
-      // определение this
-        var magnificPopup = $.magnificPopup.instance,
-                cur = magnificPopup.st.el;
-                //console.log(cur.hasClass('people-item'));
-
-              $currContainer = cur.parent();
-                var currArr;
-
-      // если это айтем от людей, то переназначить массив
-        if (cur.hasClass('people-item')) {
-            currArr = peopleArr;
-            $item.css('z-index','2');
-
-          }else {
             currArr = itemsArr;
             $peopleItems.css('z-index','2');
           }
@@ -190,24 +171,10 @@ $(document).ready(function(){
         $post.addClass(CLASS_ACTIVE);
         init($container, [], currArr);
       },
-      open: function(){
-        // Возможно, что это фиксило какой-то баг. А возможно и нет)
-
-        //$item.css('transition','all 0s');
-        // $(window).trigger('resize');
-        // return true;
-
-      },
       beforeClose: function(){
-
-      // все вернуть
         $('.main').css('display','block');
         $('.close1').removeClass('close1');
         $container.children().addClass('magpop');
-
-      // Не понимаю зачем это здесь
-        //$item.css('transition','all .5s');
-      // hash
         if(history.pushState) {
           var redirect = prefix + '/';
           history.pushState('', '', redirect);
@@ -218,13 +185,9 @@ $(document).ready(function(){
       change: function(){
         var mp = $.magnificPopup.instance,
             t = $(mp.currItem.el[0]);
-
-
-      //Закрывать статью по клику по текущему айтому
         t.removeClass('magpop');
         t.addClass('mfp-close');
         t.addClass('close1');
-      // hash
         if(history.pushState) {
           var redirect = prefix + '/#'+ t.data('id');
           window.history.pushState('', '', redirect);
@@ -232,26 +195,19 @@ $(document).ready(function(){
           changeHashWithoutScrolling(t.data('id'))
         }
 
-        if ($('.mfp-wrap').hasClass('mfp-ready')){ // когда статья открылась
-        // найти и убрать класс с крестиком с айтемов
+        if ($('.mfp-wrap').hasClass('mfp-ready')){ 
           $('.close1').removeClass('close1');
-        // всем вернуть возможность перехода по статьям
           $container.children().addClass('magpop');
-        // найти текущий выбранный элемент
           var mp = $.magnificPopup.instance,
               t = $(mp.currItem.el[0]);
-        // снять с текущего элемента возможность перехода по статьям
           t.removeClass('magpop');
-        // добавить к текущему класс с крестиком
           t.addClass('close1');
-        // Что-то связанное с оформлением, кажется opacity при переключении статей
           $('.mfp-container').addClass('mfp-figure');
           clearTimeout(to);
           to = setTimeout(function() {
             $('.mfp-container').removeClass('mfp-figure');
           }, 300);
         }
-      // Не знаю о чем это, наверное заглушка на баг
         if ($('.mfp-container').hasClass('mfp-s-ready')){
           t.removeClass('magpop');
           t.addClass('mfp-close');
@@ -262,9 +218,7 @@ $(document).ready(function(){
 
       },
       ajaxContentAdded: function(mfpResponse) {
-        // Basic FitVids Test
         $(".mfp-content").fitVids();
-        // Custom selector and No-Double-Wrapping Prevention Test
         $(".mfp-content").fitVids({ customSelector: "iframe"});
         }
 
@@ -310,7 +264,6 @@ $(document).ready(function(){
     }, 500);   
     
     $item.addClass('filtred');
-    // переключиться на другую фильтрацию: снять со всех, отдать правильному, перезаписать хэш - done
 }
   $(document).on("click", ".filter a", function(e){
     e.preventDefault();
@@ -322,7 +275,7 @@ $(document).ready(function(){
      
     if ($that.hasClass('filter_active')) flag_status = true;  
     
-    if ($post.hasClass( CLASS_ACTIVE )) {closeProgect();} // поменять хэш с проекта на фильтр 
+    if ($post.hasClass( CLASS_ACTIVE )) {closeProgect();} 
     else if (flag_status) {
       
       $('.filter_active').removeClass('filter_active');
@@ -340,7 +293,7 @@ $(document).ready(function(){
       }else{
         changeHashWithoutScrolling('')
       }
-    } //Снять выделение, сделать активным ВСЁ, очистить хэш
+    } 
     
 
     if ( !flag_status ) {
@@ -391,7 +344,6 @@ $(document).ready(function(){
     },500);
 
   if(location){
-    // console.log($(location ));
     setTimeout(function() {
       var idtop = $('.what').offset().top-200;
       $(window).scrollTop(idtop);
@@ -402,7 +354,6 @@ $(document).ready(function(){
     }, 100);
   }
   $('.people-item').addClass('people-hover')
-  //.isPerson();
 
   $('.peopleAboutItem').css('display','none')
 });
